93989e2c97351a6e7f4d3f55b23fa5c853d307ab
Robert Dodier <robert.dodier@gmail.com>
2024-08-13 10:46:53 -0700

    Initial implementation of simplifying operations for bra and ket.
     * qm_simplifying.mac: simplification rules
     * qm_simplifying.lisp: display properties
     * maxima-session-qm_simplifying.txt: session log showing examples

---
+++ qm_simplifying.lisp
@@ -0,0 +1,14 @@
+;; arrange for bra(a) to be displayed as <a|
+
+(setf (get '$bra 'dissym) '((#\<) #\|))
+(setf (get '$bra 'dimension) 'dimension-match)
+
+;; arrange for ket(a) to be displayed as |a>
+
+(setf (get '$ket 'dissym) '((#\|) #\>))
+(setf (get '$ket 'dimension) 'dimension-match)
+
+;; arrange for dagger(a) to be displayed as a†
+
+(setf (get '$dagger 'dissym) '(#\†))
+(setf (get '$dagger 'dimension) 'dimension-postfix)
