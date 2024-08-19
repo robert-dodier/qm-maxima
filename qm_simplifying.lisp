345aa093766954ed0929225959144f0c0dd73321
Robert Dodier <robert.dodier@gmail.com>
2024-08-18 21:42:56 -0700

    Rules for simplifying (j, m) bras and kets, and reworking display code to avoid formatter property, since that has undesired side effects (changes result of op, args, and part).

--- qm_simplifying.lisp
+++ qm_simplifying.lisp
@@ -13,30 +13,49 @@
 ;;     |a>
 ;;        (...)
 
-;; the formatter function is called before op, args, and part (unless inpart = true),
-;; so defining a formatter function for display has the side effect of changing
-;; op(ket[...](a)), args(ket[...](a)), etc. Unless that makes sense, setting up the
-;; display via the formatter property should be considered a stop-gap measure.
+(defun reform-mqapply-bra-or-ket (form)
+  (let ((bra-or-ket ($op ($op form))))
+    (list '(mqapply array) (cons `(,bra-or-ket) (rest (rest form))) (cons '(mprogn) (rest (second form))))))
 
-(defun form-mqapply (form)
-  (if (and (member 'array (car (second form))) (eq (caar (second form)) '$ket))
-    (nformat (list '(mqapply array) (cons '($ket) (rest (rest form))) (cons '(mprogn) (rest (second form)))))
-    form))
+(defun dimension-mqapply-bra-or-ket (form result)
+  (if (and (not (member 'array (car form))) (member 'array (car (second form))) (member (caar (second form)) '($bra $ket)))
+    (dimension-array (reform-mqapply-bra-or-ket form) result)
+    (dimension-function form result)))
 
-(setf (get 'mqapply 'formatter) 'form-mqapply)
+;; Maybe we should try to cope with the possibility that there
+;; might be a display function for MQAPPLY relevant in some other context ...
+;; as it stands, any other definition is just clobbered here.
+
+(setf (get 'mqapply 'dimension) 'dimension-mqapply-bra-or-ket)
 
 ;; arrange for braket(bra(a), ket(b)) to be displayed as <a|b>
-
-;; above comment about the formatter property applies here as well.
-
-(defun form-braket (form) `((braket) ((vbar) ,(second (second form)) ,(second (third form)))))
-(setf (get '$braket 'formatter) 'form-braket)
 
 (setf (get 'braket 'dissym) '((#\<) #\>))
 (setf (get 'braket 'dimension) 'dimension-match)
 
 (setf (get 'vbar 'dissym) '(#\|))
 (setf (get 'vbar 'dimension) 'dimension-infix)
+
+(defun reform-braket (form)
+  (let*
+    ((args-braket ($args form))
+     (bra ($first args-braket))
+     (ket ($second args-braket))
+     (op-bra ($op bra))
+     (op-ket ($op ket)))
+    (if (and ($atom op-bra) ($atom op-ket))
+      `((braket) ((vbar) ,($first bra) ,($first ket)))
+      (if (and ($mapatom op-bra) ($mapatom op-ket) (every 'alike1 (rest op-bra) (rest op-ket)))
+        `((mqapply array) ((braket) ((vbar) ,($first bra) ,($first ket))) ((mprogn) ,@(rest op-bra)))
+        `((braket) ((vbar) ,bra ,ket))))))
+
+(defun dimension-braket (form result)
+  (let*
+    ((reformed (reform-braket form))
+     (helper (get (caar reformed) 'dimension)))
+    (funcall helper reformed result)))
+
+(setf (get '$braket 'dimension) 'dimension-braket)
 
 ;; arrange for dagger(a) to be displayed as a†
 
@@ -47,15 +66,11 @@
 ;; otherwise display as a function (which is the current default).
 ;; note a* has the asterisk on the same line as a; a^* puts the asterisk too high, I believe.
 
-;; above comment about the formatter property applies here as well.
-
-(defun form-conjugate (form)
+(defun reform-conjugate (form)
   (if ($mapatom (second form))
     `((conjugate) ,(second form))
     ;; hmm, do we need to ensure arguments are formatted? or it will happen automatically?
     form))
-
-(setf (get '$conjugate 'formatter) 'form-conjugate)
 
 (setf (get 'conjugate 'dissym) '(#\*))
 (setf (get 'conjugate 'dimension) 'dimension-postfix)
